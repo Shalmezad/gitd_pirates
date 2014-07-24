ebfeb0d5301c51589b0bf3f0880d829fea82e115
Richard Wardin <shalmezad@gmail.com>
2014-07-24 15:59:30 -0400

    Cleaned up and refactored a bit

---
+++ source/states/MenuState.hx
@@ -0,0 +1,25 @@
+package states;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+
+class MenuState extends FlxState
+{
+	override public function create():Void
+	{
+		super.create();
+	}
+
+	override public function destroy():Void
+	{
+		super.destroy();
+	}
+
+	override public function update():Void
+	{
+		super.update();
+	}	
+}
