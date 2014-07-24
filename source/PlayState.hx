fce9f94daaff823062c055c6ed8f77aa9feb90ed
Richard Wardin <shalmezad@gmail.com>
2014-07-24 15:56:13 -0400

    Refactored maps to XML to allow scene objects

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -13,16 +13,35 @@ import openfl.Assets;
 
 class PlayState extends FlxState
 {
+	private static var TILE_WIDTH:Int = 20;
+	private static var TILE_HEIGHT:Int = 20;
+
+	private static var MAP_DATA:Xml = null;
+
+	private var currentMap:Xml = null;
 	private var map:FlxTilemap;
 
 	override public function create():Void
 	{
 		super.create();
+		if(MAP_DATA == null)
+		{
+				MAP_DATA = Xml.parse(Assets.getText("assets/data/maps.xml"));
+		}
+		for(m in MAP_DATA.firstElement().elements())
+		{
+			if(m.get("id") == Std.string(1))
+			{
+				currentMap = m;
+			}
+		}
 		map = new FlxTilemap();		
-		map.loadMap(Assets.getText("assets/data/map1.csv"), "assets/images/tileset.png", 20,20,FlxTilemapAutoTiling.OFF,0,1,2);
+		map.loadMap(currentMap.get("data"), "assets/images/tileset.png", TILE_WIDTH,TILE_HEIGHT,FlxTilemapAutoTiling.OFF,0,1,2);
 		add(map);
 
-		var player:FlxSprite = new FlxSprite(40,40);
+		var player:FlxSprite = new FlxSprite();
+		player.x = Std.parseInt(currentMap.elementsNamed("player").next().get("x")) * TILE_WIDTH;
+		player.y = Std.parseInt(currentMap.elementsNamed("player").next().get("y")) * TILE_HEIGHT;
 		player.loadGraphic("assets/images/player1.png");
 		add(player);
 	}
@@ -37,3 +56,33 @@ class PlayState extends FlxState
 		super.update();
 	}	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
